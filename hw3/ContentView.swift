eb26acfebba7c6247f8c07e9a4d59518de6e6ba3
Henry <henry0523ho.3@gmail.com>
2022-10-13 09:45:39 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by henry on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw3/hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  hw3App.swift
+//  hw3
+//
+//  Created by henry on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
